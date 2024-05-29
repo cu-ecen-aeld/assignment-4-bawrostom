dcecc4f65d6a1e94f1f9e29dc68fa3553a0ce30e
bawrostom <rostombaw2019@gmail.com>
2024-05-30 00:35:17 +0200

    Threading problem solved

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -10,11 +10,25 @@
 
 void* threadfunc(void* thread_param)
 {
+    struct thread_data* thread_args = (struct thread_data*) thread_param;
 
     // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
     // hint: use a cast like the one below to obtain thread arguments from your parameter
     //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
-    return thread_param;
+
+    usleep(thread_args->wait * 1000);
+    if (pthread_mutex_lock(thread_args->mutex) != 0){
+        return thread_args;
+    }
+
+    usleep(thread_args->release * 1000);
+
+    if (pthread_mutex_unlock(thread_args->mutex) != 0){
+        return thread_args;
+    }
+
+    thread_args->thread_complete_success = true;
+    return thread_args;
 }
 
 
@@ -28,6 +42,20 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
      *
      * See implementation details in threading.h file comment block
      */
-    return false;
-}
 
+    struct thread_data* thread_param = (struct thread_data *) malloc(sizeof(struct thread_data));
+
+    thread_param->wait = wait_to_obtain_ms;
+    thread_param->release = wait_to_release_ms;
+    thread_param->mutex = mutex;
+    thread_param->thread_complete_success = false;
+    
+
+
+    if (pthread_create(thread, NULL, &threadfunc, (struct thread_data* ) thread_param) !=0){
+        perror("Failed on initializing producers");
+        return false;
+    }
+
+    return true;
+}
